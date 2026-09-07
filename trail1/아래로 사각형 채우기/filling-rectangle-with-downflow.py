4e0a618448f97360e1c600dfffdf944569743c2a
HYUN-08 <khs_2085@naver.com>
2026-09-07 11:55:14 +0900

    [Passed] 아래로 사각형 채우기 (49ms, 16MB)

---
+++ trail1/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+arr = [[0]*n for i in range(n)]
+
+num = 1
+
+for i in range(n):
+        for j in range(n):
+            arr[j][i] = num
+            num += 1
+
+for row in arr:
+    for elem in row:
+        print(elem, end=" ")
+    print()
