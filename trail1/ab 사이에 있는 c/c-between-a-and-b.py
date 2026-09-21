e3b11ab84a9cc905d9ac415ea18c72af748bc1c7
HYUN-08 <khs_2085@naver.com>
2026-09-21 15:01:56 +0900

    [Passed] ab 사이에 있는 c (55ms, 16MB)

---
+++ trail1/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+
+flag = False
+for i in range(a, b+1):
+    if i % c == 0:
+        flag = True
+        break
+
+if flag:
+    print("YES")
+else:
+    print("NO")
