25f79aa01e9dccb01f56aa575acf3f3bb8c313ae
HYUN-08 <khs_2085@naver.com>
2026-09-21 17:39:29 +0900

    [Passed] 함수를 이용한 윤년 판별 (46ms, 16MB)

---
+++ trail2/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,16 @@
+y = int(input())
+
+# Please write your code here.
+def leap_year(y):
+    if y % 100 == 0 and y % 400 != 0:
+        return False
+    elif y % 4 != 0 :
+        return False
+    else:
+        return True
+
+
+if leap_year(y):
+    print("true")
+else:
+    print("false")
