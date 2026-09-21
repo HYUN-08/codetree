8398d09dbdc09f3ea7eb13502f9359b72ba78b96
HYUN-08 <khs_2085@naver.com>
2026-09-21 17:04:15 +0900

    [Passed] 짝수이면서 합이 5의 배수인 수 (46ms, 16MB)

---
+++ trail2/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+# Please write your code here.
+def check(n):
+    return n % 2 == 0 and (n // 10 + n % 10) % 5 == 0
+    
+if check(n):
+    print("Yes")
+else:
+    print("No")
